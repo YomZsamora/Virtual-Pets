af59ba1d1eb046d76866cf09b5de8d8f51ecddc1
yomZsamora <samora.y@adzumi.co.ke>
2018-05-22 17:06:17 +0300

    App Setup Finalized.

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: ./build/install/virtual-pets/bin/virtual-pets
---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,60 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+
+public class PersonTest {
+
+	@Rule
+	public DatabaseRule database = new DatabaseRule();
+
+	@Test
+	public void person_instantiatesCorrectly_true() {
+		Person testPerson = new Person("Henry", "henry@henry.com");
+		assertEquals(true, testPerson instanceof Person);
+	}
+
+	@Test
+	public void getName_personInstantiatesWithName_Henry() {
+		Person testPerson = new Person("Henry", "henry@henry.com");
+		assertEquals("Henry", testPerson.getName());
+	}
+
+	@Test
+	public void getName_personInstantiatesWithEmail_String() {
+		Person testPerson = new Person("Henry", "henry@henry.com");
+		assertEquals("henry@henry.com", testPerson.getEmail());
+	}
+
+	@Test
+	public void equals_returnsTrueIfNameAndEmailAreSame_true() {
+		Person testPerson = new Person("Henry", "henry@henry.com");
+		Person anotherPerson = new Person("Henry", "henry@henry.com");
+		assertTrue(testPerson.equals(anotherPerson));
+	}
+
+	@Test
+	public void save_insertsObjectIntoDatabase_Person() {
+		Person testPerson = new Person("Henry", "henry@henry.com");
+		testPerson.save();
+		assertEquals(true, Person.all().get(0).equals(testPerson));
+	}
+
+	@Test
+	public void all_returnsAllInstancesOfPerson_true() {
+		Person firstPerson = new Person("Henry", "henry@henry.com");
+		firstPerson.save();
+		Person secondPerson = new Person("Harriet", "harriet@harriet.com");
+		secondPerson.save();
+		assertEquals(true, Person.all().get(0).equals(firstPerson));
+		assertEquals(true, Person.all().get(1).equals(secondPerson));
+	}
+
+	@Test
+	public void save_assignsIdToObject() {
+		Person testPerson = new Person("Henry", "henry@henry.com");
+		testPerson.save();
+		Person savedPerson = Person.all().get(0);
+		assertEquals(testPerson.getId(), savedPerson.getId());
+	}
+
+}
